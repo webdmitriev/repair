c799e37c0e3cfdf8de569d1839faa7f11890e7ea
webdmitriev <webdmitriev@gmail.com>
2025-11-07 19:16:20 +0300

    block-03 frontand is ready

--- pages/page-development.php
+++ pages/page-development.php
@@ -79,5 +79,32 @@ $url = get_template_directory_uri();
     </div>
   </section>
 
+  <section class="block-03 pos-r w-100p z-section" id="block-03">
+    <div class="container">
+      <div class="block__title pos-r df-sp-ce w-100p" data-title="Как мы работаем">
+        <h2 class="h2">Как мы работаем</h2>
+        <a href="#" class="btn btn-accent">Как мы работаем</a>
+      </div>
+
+      <div class="line-wrap df-sp-fs w-100p">
+        <div class="reused block-image"><img src="https://stiralka-mamayka.ru/wp-content/uploads/2025/10/image-01.jpg" alt="alto" class="block-image__img" /></div>
+        <div class="reused block-content">
+          <div class="hide-text">
+            <p class="text">Сломался помощник по хозяйству? Не беда! <br/>Мы занимаемся ремонтом стиральных машин прямо на Мамайке, чтобы ваш привычный уклад жизни в нашем солнечном районе ничто не нарушало. <br/><br/>Как мы это делаем? <br/>— Работаем в удобное для вас время — подстроимся под ваш график. <br/>— Предлагаем честные цены без накруток и только необходимый ремонт. <br/>— Относимся к вам и вашей технике с заботой и уважением. <br/><br/>Наша цель — не просто починить, а вернуть в ваш дом уют и спокойствие!</p>
+            <p class="hide-text__btn">Читать далее</p>
+          </div>
+
+          <div class="block-item">
+            <img src="https://stiralka-mamayka.ru/wp-content/uploads/2025/10/icon-01-default.svg" alt="" class="block-item__img" loading="lazy" decoding="async" />
+            <div class="block-item__content">
+              <p class="content__label">Вызов специалиста на дом</p>
+              <p class="content__descr">Мы предлагаем бесплатный вызов мастера на дом в течении 40 минут или удобное для вас время в районе Мамайка г. Сочи</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
 <?php
 get_footer();
