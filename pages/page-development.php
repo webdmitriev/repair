6e3fb1abdc7e82d0963c2e8685fc42d6a11fee2f
webdmitriev <webdmitriev@gmail.com>
2025-11-07 17:32:31 +0300

    block-01 frontand is ready

--- pages/page-development.php
+++ pages/page-development.php
@@ -5,14 +5,15 @@
 
 
 get_header();
-
 $url = get_template_directory_uri();
-$image_base64 = 'data:image/gif;base64,R0lGODlhBwAFAIAAAP///wAAACH5BAEAAAEALAAAAAAHAAUAAAIFjI+puwUAOw==';
+
 ?>
 
-<main class="block-01">
-  <div class="container">
-    <h1 class="h1">Ремонт стиральных машин <br/>на Мамайке в Сочи</h1>
+<main class="block-01 pos-r df-sp-st w-100p z-section">
+  <img src="<?= $url; ?>/assets/img/block-01/bg-1920.jpg" alt="alto" class="background-image" />
+  <div class="background-shadow" style="background-color: rgba(0,0,0,0.3);"></div>
+  <div class="container container-main">
+    <h1 class="h1">Ремонт бытовой техники <br/>в&nbsp;Ростове-на-Дону</h1>
     <div class="block-items"><div class="block-item">— Выезд мастера в течении 40 минут</div></div>
     <button class="btn btn-popup-call">Вызвать мастера</button>
   </div>
