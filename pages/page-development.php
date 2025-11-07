d8d8c85cde493bb4b76cc1fb0dd23cc0a61ca659
webdmitriev <webdmitriev@gmail.com>
2025-11-07 15:12:39 +0300

    changed block-01

--- pages/page-development.php
+++ pages/page-development.php
@@ -11,14 +11,13 @@ $image_base64 = 'data:image/gif;base64,R0lGODlhBwAFAIAAAP///wAAACH5BAEAAAEALAAAA
 ?>
 
 <main class="block-01">
-  <div class="container df-sp-st">
-    <div class="block-content">
-      <h1 class="h1">Ремонт бытовой техники и&nbsp;компьютеров от&nbsp;лучших профессионалов <br/>в&nbsp;Ростове-на-Дону!</h1>
-      <p class="sub_title">В&nbsp;90% случаев чиним бытовую технику в&nbsp;день обращения! Все что вам нужно&nbsp;&mdash; позвонить нам, и&nbsp;мастер сразу выедет к&nbsp;вам.</p>
-      <button class="btn icon-btn-arrow-01">Вызвать мастера</button>
-    </div>
-    <img src="<?= $url; ?>/assets/img/block-01/image-01.jpeg" alt="alto" class="block-image" />
+  <div class="container">
+    <h1 class="h1">Ремонт стиральных машин <br/>на Мамайке в Сочи</h1>
+    <div class="block-items"><div class="block-item">— Выезд мастера в течении 40 минут</div></div>
+    <button class="btn btn-popup-call">Вызвать мастера</button>
   </div>
+
+  <div class="container"><address class="address-icon">Мамайка, г. Сочи</address></div>
 </main>
 
 <?php
